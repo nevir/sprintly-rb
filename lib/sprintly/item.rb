238151f6fcdef04bab77b061d36c8f15455423be
Ian MacLeod <ian@nevir.net>
2012-12-23 22:53:55 -0800

    Item attributes filled in

--- lib/sprintly/item.rb
+++ lib/sprintly/item.rb
@@ -7,9 +7,38 @@ class Sprintly::Item
 
   # Attributes
   # ----------
-  attribute :number, read_only: true
+  ### Identity
+  attribute :number,               read_only: true
+  attribute :product,    :Product, read_only: true
+  attribute :created_by, :Person,  read_only: true
+
+  ### Content
+  attribute :who
+  attribute :what
+  attribute :why
   attribute :title
-  attribute :type
-  attribute :product, as: :Product
+  attribute :description
+
+  ### Statuses
+  attribute :type,        :Symbol
+  attribute :status,      :Symbol
+  attribute :archived?,   :Symbol
+  attribute :assigned_to, :Person
+  attribute :score,       :Symbol
+
+  ### Metadata
+  attribute :tags,                 read_only: true
+  attribute :short_url,            read_only: true
+  attribute :created_at,    :Time, read_only: true
+  attribute :last_modified, :Time, read_only: true
+
+  # An email address for a particular portion of the item.
+  #
+  # Known options:
+  # `files`:      Email attachments to the item to this address.
+  # `discussion`: Email additional comments to this address.
+  def email_for(task)
+    @email[task.to_s] if @email
+  end
 
 end
--- spec/sprintly/item_spec.rb
+++ spec/sprintly/item_spec.rb
@@ -7,18 +7,86 @@ describe Sprintly::Item do
 
   describe "Attributes" do
 
-    it "should have a number" do
-      subject.number.should eq(1)
+    describe "identity" do
+
+      it "should have a number" do
+        subject.number.should eq(1)
+      end
+
+      it "should embed a Product" do
+        subject.product.should be_a(Sprintly::Product)
+        subject.product.id.should eq(8094)
+      end
+
+      it "should embed an author" do
+        subject.created_by.should be_a(Sprintly::Person)
+        subject.created_by.id.should eq(9708)
+      end
+
     end
 
-    it "should have a type" do
-      subject.type.should eq("story")
+    describe "content" do
+
+      it "should have a who (if a story)" do
+        subject.who.should eq("evil genius")
+      end
+
+      it "should have a what (if a story)" do
+        subject.what.should eq("to recruit a multitude of henchmen")
+      end
+
+      it "should have a why (if a story)" do
+        subject.why.should eq("I am one step closer to taking over the world")
+      end
+
+      it "should have a title" do
+        subject.title.should eq(
+          "As an evil genius, I want to recruit a multitude of henchmen so that I am one step closer to taking over the world."
+        )
+      end
+
     end
 
-    it "should have a title" do
-      subject.title.should eq(
-        "As an evil genius, I want to recruit a multitude of henchmen so that I am one step closer to taking over the world."
-      )
+    describe "statuses" do
+
+      it "should have a type" do
+        subject.type.should eq(:story)
+      end
+
+      it "should have a status" do
+        subject.status.should eq(:backlog)
+      end
+
+      it "should indicate whether it is archived" do
+        subject.should_not be_archived
+      end
+
+      pending "should indicate who it is assigned to"
+
+      it "should have a score" do
+        subject.score.should eq(:XL)
+      end
+
+    end
+
+    describe "metadata" do
+
+      pending "should list its tags"
+
+      [:files, :discussion].each do |kind|
+        it "should expose email addresses for #{kind} updates" do
+          subject.email_for(kind).should match(%r{#{kind}-\d+@items.sprint.ly})
+        end
+      end
+
+      it "should have a created timestamp" do
+        subject.created_at.should be_a(Time)
+      end
+
+      it "should have a last modified timestamp" do
+        subject.last_modified.should be_a(Time)
+      end
+
     end
 
   end
