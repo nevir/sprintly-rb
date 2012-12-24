093877d3ad4a04ad291fa91c27bee5496ddfb350
Ian MacLeod <ian@nevir.net>
2012-12-23 16:17:40 -0800

    Super basic item model; unfinished

---
+++ lib/sprintly/item.rb
@@ -0,0 +1,10 @@
+class Sprintly::Item
+  include Sprintly::Model
+
+  # Attributes
+  # ----------
+  attribute :number
+  attribute :title
+  attribute :type
+
+end
---
+++ spec/sprintly/item_spec.rb
@@ -0,0 +1,26 @@
+describe Sprintly::Item do
+  include_context "common client"
+
+  subject do
+    described_class.new(client.api.get_item(8094, 1), client)
+  end
+
+  describe "Attributes" do
+
+    it "should have a number" do
+      subject.number.should eq(1)
+    end
+
+    it "should have a type" do
+      subject.type.should eq("story")
+    end
+
+    it "should have a title" do
+      subject.title.should eq(
+        "As an evil genius, I want to recruit a multitude of henchmen so that I am one step closer to taking over the world."
+      )
+    end
+
+  end
+
+end
